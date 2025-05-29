4bc8cf94039d5067b99549a2c27cc2e10f99e557
Bommelom <81819254+Bommelom1@users.noreply.github.com>
2025-05-29 11:13:07 +0200

    new draft

--- app_August/models/courses_at_year.py
+++ app_August/models/courses_at_year.py
@@ -2,15 +2,15 @@ from database import db_connection
 from models.category import Category
 
 class CourseAtYear:
-    def __init__(self, id, name, term):
+    def __init__(self, id, term, blok):
         self.id = id
-        self.name = name
+        self.blok = blok
         self.term = term
 
 def list_courses_at_year():
     conn = db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT course_ID, course_name, term FROM COURSE_AT_YEAR')
+    cur.execute('SELECT KURSUS_ID, term, blok FROM COURSE_AT_YEAR')
     db_CoursesAtYear = cur.fetchall()
     CoursesAtYear = []
     for db_CourseAtYear in db_CoursesAtYear:
