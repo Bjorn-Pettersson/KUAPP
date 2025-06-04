21a277fba04670a4510847c46b447a29cb5bb828
Bommelom <81819254+Bommelom1@users.noreply.github.com>
2025-06-04 15:18:45 +0200

    deleting bjorn as well

--- app_Bjorn/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    course_name = db.Column(db.String(120), nullable=False)
-    description = db.Column(db.Text)
-    credits = db.Column(db.Integer)
